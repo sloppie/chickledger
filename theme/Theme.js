8c354c80ee2e8077842b1bd4271044e2f0a0a256
sloppie_ <dgitau75@gmail.com>
2019-09-13 23:05:01 +0300

    Added the theme file to the source control

---
+++ theme/Theme.js
@@ -0,0 +1,25 @@
+/**
+ * This class contains the constannts for the application's theme color variables
+ * This helps make it easy to modify their values due to them not being hardcoded everywhere
+ */
+export default class Theme{
+  static PRIMARY_COLOR = "#546e7a";
+  static PRIMARY_COLOR_DARK = "#819ca9";
+  static PRIMARY_COLOR_LIGHT = "#29434e";
+
+  static SECONDARY_COLOR = "#5d4037";
+  static SECONDARY_COLOR_DARK = "#321911";
+  static SECONDARY_COLOR_LIGHT = "#8b6b61";
+
+  static HEADER_COLOR = "#000";
+  static SUBTITLE_COLOR = "#333";
+  static PARAGRAPH_COLOR = "#fff";
+  static COMMENT_COLOR = "#777"
+
+  static GRAY = "#f3f3f3"
+
+  static HEADER_WEIGHT = "700";
+  static SUBTITLE_WEIGHT = "500";
+  static NORMAL_WEIGHT = "300";
+  static LIGHT_WEIGHT = "200";
+}
